212d7de2c0a3470171770ade74a51e64c6f44e14
Gryphon M <71658735+GryphonM@users.noreply.github.com>
2023-02-04 13:42:03 -0800

    Fancy Linked List

---
+++ BoidBox/BoidBox/List.cpp
@@ -0,0 +1,48 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	List.cpp
+// Author(s):	Gryphon McLaughlin (gryphon.mclaughlin)
+// Project:		GAM 150 Game Project
+// Course:		GAM 150
+//
+//------------------------------------------------------------------------------
+
+#include "List.h"
+#include "Object.h"
+
+ObjectList::ObjectList() : list() {}
+ObjectList::~ObjectList()
+{
+	Clear();
+}
+
+void ObjectList::PushBack(Object* obj) { list.push_back(obj); }
+void ObjectList::PushFront(Object* obj) { list.push_front(obj); }
+Object* ObjectList::Back() { return list.back(); }
+Object* ObjectList::Front() { return list.front(); }
+Object* ObjectList::PopBack() { Object* back = list.back(); list.pop_back(); return back; }
+Object* ObjectList::PopFront() { Object* front = list.front(); list.pop_front(); return front; }
+void ObjectList::Erase(Object* obj) { list.erase(std::find(list.begin(), list.end(), obj)); }
+void ObjectList::Clear()
+{
+	int size = list.size();
+	for (int i = 0; i < size; ++i)
+		delete PopBack();
+}
+Object* ObjectList::operator[](int index)
+{
+	if (index > list.size() || index < 0)
+		return NULL;
+	auto it = list.begin();
+	for (int i = 0; i < index; ++i)
+		++it;
+	return *it;
+}
+
+int ObjectList::Size() { return list.size(); }
+bool ObjectList::IsEmpty() { return list.empty(); }
+
+
+void ObjectListAdd(ObjectList* list, Object* obj) { list->PushBack(obj); }
+
+void ObjectListRemove(ObjectList* list, Object* obj) { list->Erase(obj); }
---
+++ BoidBox/BoidBox/List.h
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	List.h
+// Author(s):	Gryphon McLaughlin (gryphon.mclaughlin)
+// Project:		GAM 150 Game Project
+// Course:		GAM 150
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+struct Object;
+#include <list>
+
+class ObjectList
+{
+public:
+	ObjectList();
+	~ObjectList();
+
+	void PushBack(Object* obj);
+	void PushFront(Object* obj);
+	Object* Back();
+	Object* Front();
+	Object* PopBack();
+	Object* PopFront();
+	Object* operator[](int index);
+	void Erase(Object* obj);
+	void Clear();
+
+	int Size();
+	bool IsEmpty();
+private:
+	std::list<Object*> list;
+};
+
+void ObjectListAdd(ObjectList* list, Object* obj);
+void ObjectListRemove(ObjectList* list, Object* obj);
