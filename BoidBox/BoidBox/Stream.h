bb30b15fe597180e45db9a515a4bcadfcf462523
Gryphon M <71658735+GryphonM@users.noreply.github.com>
2023-02-11 02:00:10 -0800

    I did things?

---
+++ BoidBox/BoidBox/Stream.h
@@ -0,0 +1,12 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Stream.h
+// Author(s):	Gryphon McLaughlin (gryphon.mclaughlin)
+// Project:		GAM 150 Game Project
+// Course:		GAM 150
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+
