d7e7118c83a4c05e3642b02d06516264c2e3873b
redarsonal <youngt0318@gmail.com>
2023-02-26 20:06:14 -0800

    better actioin list
    uhfcuwoh

--- BoidBox/BoidBox/ActionList.cpp
+++ BoidBox/BoidBox/ActionList.cpp
@@ -8,7 +8,11 @@
 //------------------------------------------------------------------------------
 
 #include "ActionList.h"
+#include "Vector2D.h"
+#include "DGL.h"
 #include "Math.h"
+
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 typedef enum
@@ -31,21 +35,63 @@ struct ActionList
     float currentTime;
     int currentObject;
 
-    DGL_Vec2 startPos;
-    DGL_Vec2 endPos;
+    Vector2D startPos;
+    Vector2D endPos;
     DGL_Color startColor;
     DGL_Color endColor;
     float startVal;
     float endVal;
 };
 
+///////////////////////////////////////////////////////////////////////
 
-void initActionList(ActionList* action)
+// these could prolly go to math.h
+
+float AddSine(float percent)
+{
+    return (float)sin(percent * M_PI * 0.5f);
+}
+
+float AddSqrt(float percent)
+{
+    return sqrtf(percent);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+void MoveUpdate(Vector2D* objectPos, Vector2D start, Vector2D end, float percent)
+{
+    percent = AddSine(percent);
+
+    // interp
+    objectPos->X(start.X() * (end.X() - start.X()) * percent);
+    objectPos->Y(start.Y() * (end.Y() - start.Y()) * percent);
+}
+
+void RotateUpdate(float* objectRotation, float start, float end, float percent)
+{
+    percent = AddSine(percent);
+    *objectRotation = start + (end - start) * percent;
+}
+
+void ScaleUpdate(float* objectScale, float start, float end, float percent)
+{
+    *objectScale = start + (end - start) * percent;
+}
+
+// color update requires object stuff with color attached
+
+void FadeUpdate(float* objectAlpha, float start, float end, float percent)
+{
+    *objectAlpha = start + (end - start) * percent;
+}
+
+void InitActionList(ActionList* action)
 {
 	memset(action, 0, sizeof(ActionList) * ACTIONLIST_SIZE);
 }
 
-void updateActionList(ActionList* action, float dt)
+void UpdateActionList(ActionList* action, float dt)
 {
 	for (int i = 0; i < ACTIONLIST_SIZE; ++i)
 	{
@@ -114,7 +160,7 @@ ActionList* FindOpenAction(ActionList* action)
 }
 
 //delay is bad time but i havent decided what yet lol
-void initAction(ActionList* action, int object, float useTime, float delay, ActionTypes type)
+void InitAction(ActionList* action, int object, float useTime, float delay, ActionTypes type)
 {
     action->isInUse = true;
     action->currentType = type;
@@ -123,7 +169,7 @@ void initAction(ActionList* action, int object, float useTime, float delay, Acti
     action->currentObject = object;
 }
 
-void destroyAction(ActionList* action, int object, float duration, float delay)
+void AddDeleteAction(ActionList* action, int object, float duration, float delay)
 {
     ActionList* actionlist = FindOpenAction(action);
     if (!actionlist)
@@ -131,5 +177,65 @@ void destroyAction(ActionList* action, int object, float duration, float delay)
         return;
     }
 
-    initAction(actionlist, object, duration, delay, DestroyAction);
+    InitAction(actionlist, object, duration, delay, DestroyAction);
+}
+
+void AddMoveAction(ActionList* action, int object, Vector2D startPos, Vector2D endPos, float time, float delay)
+{
+    ActionList* actionlist = FindOpenAction(action);
+    if (!actionlist)
+    {
+        return;
+    }
+    InitAction(actionlist, object, time, delay, MoveAction);
+    actionlist->startPos = startPos;
+    actionlist->endPos = endPos;
+}
+
+void AddRotateAction(ActionList* action, int object, float start, float end, float time, float delay)
+{
+    ActionList* actionlist = FindOpenAction(action);
+    if (!actionlist)
+    {
+        return;
+    }
+    InitAction(actionlist, object, time, delay, RotateAction);
+    actionlist->startVal = start;
+    actionlist->endVal = end;
+}
+
+void AddScaleAction(ActionList* action, int object, float start, float end, float time, float delay)
+{
+    ActionList* actionlist = FindOpenAction(action);
+    if (!actionlist)
+    {
+        return;
+    }
+    InitAction(actionlist, object, time, delay, ScaleAction);
+    actionlist->startVal = start;
+    actionlist->endVal = end;
+}
+
+void AddColorAction(ActionList* action, int object, DGL_Color startingColor, DGL_Color endingColor, float time, float delay)
+{
+    ActionList* actionlist = FindOpenAction(action);
+    if (!actionlist)
+    {
+        return;
+    }
+    InitAction(actionlist, object, time, delay, ColorAction);
+    actionlist->startColor = startingColor;
+    actionlist->endColor = endingColor;
+}
+
+void AddFadeAction(ActionList* action, int object, float start, float end, float time, float delay)
+{
+    ActionList* actionlist = FindOpenAction(action);
+    if (!actionlist)
+    {
+        return;
+    }
+    InitAction(actionlist, object, time, delay, FadeAction);
+    actionlist->startVal = start;
+    actionlist->endVal = end;
 }
