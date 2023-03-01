7b3f5f7548df817c036b006b1eb6a49c6b56df8b
Gryphon M <71658735+GryphonM@users.noreply.github.com>
2023-02-28 20:46:36 -0800

    WHO DID THIS

--- BoidBox/BoidBox/OpenScene.cpp
+++ BoidBox/BoidBox/OpenScene.cpp
@@ -51,7 +51,8 @@ Scene* OpenSceneGetInstance() { return &(instance.base); }
 Engine::ErrorCode OpenSceneLoad(void)
 {
 	squareMesh = SquareMesh(0.5f, 0.5f, 1.0f, 1.0f, "Square Mesh", { 0.0f, 0.0f, 0.0f, 0.0f });
-	pos = CreateTransform(Vector2D(), Vector2D(200.0, 200.0f));	source = CreateSpriteSource();
+	pos = CreateTransform(Vector2D(), Vector2D(200.0, 200.0f));	
+	source = CreateSpriteSource();
 	source = CreateSpriteSource();
 	LoadSpriteSourceTexture(source, 1, 1, "./Assets/containmentbreached.png");
 	instance.OpenSprite = CreateSprite();
